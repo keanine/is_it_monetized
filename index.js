f58d84f6b3502ec69a87c5719195262646a66a60
keanine <keanen195@gmail.com>
2022-08-16 01:41:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function start()
         for (let i = 0; i < output.length; i++)
         {
             let title = output[i].title;
+            console.log(title);
             element.innerHTML += '<div class="reviewCard"><img class="reviewThumbnail" alt="Game Boxart" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/f66a0c26ea3a640283a18af4915c577a.png"><p class="reviewTitle">' + title + '</p><p class="reviewRating">Low</p>	</div>';
         }
     }).catch(err => console.error(err));
