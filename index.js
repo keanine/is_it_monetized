b58a18bbbd7820b7a76ae62ec129ce8945271302
keanine <keanen195@gmail.com>
2022-08-16 15:05:46 +0100

    Removed temp cards, added onclick to cards

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function start()
             let rating = output[i].rating;
             let boxart = output[i].boxart;
             
-            element.innerHTML += '<div class="reviewCard"><img class="reviewThumbnail" alt="Game Boxart" src="' + boxart + '"><p class="reviewTitle">' + title + '</p><p class="reviewRating">' + rating + '</p>	</div>';
+            element.innerHTML += '<div class="reviewCard"  onclick="goto(\'game.html\', \''+ output.gameID + '\')"><img class="reviewThumbnail" alt="Game Boxart" src="' + boxart + '"><p class="reviewTitle">' + title + '</p><p class="reviewRating">' + rating + '</p>	</div>';
         }
     }).catch(err => console.error(err));
 }
