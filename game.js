e3f38eb31f393f06eeca6bda8db0ace1a2e39582
keanine <keanen195@gmail.com>
2022-08-16 22:01:03 +0100

    Speedy gains

--- game.js
+++ game.js
@@ -1,19 +1,24 @@
+
 function start()
 {    
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const gameID = urlParams.get('id');
+    
+    //var svgItem = document.getElementById("svgCheckmark");
+    //console.log(svgItem);
+	//svgItem.setAttribute("fill", "#c7c7c7");
 
-    const checkmarkCollection = document.getElementsByClassName("checkmark");
-    for (let i = 0; i < checkmarkCollection.length; i++) {
-        changeCheckmarkColor(checkmarkCollection[i], "#c7c7c7");
-        //changeCheckmarkColor(collection[i], "#ff9900");
-    }
+     const checkmarkCollection = document.getElementsByClassName("checkmark");
+    // console.log(checkmarkCollection);
+    // for (let i = 0; i < checkmarkCollection.length; i++) {
+    //     changeCheckmarkColor(checkmarkCollection[i], "#c7c7c7");
+    // }
 
     fetch('./data/game_database.json')
     .then(result => result.json())
     .then((allGameData) => {
-        console.log('All Game Data: ', allGameData);
+        //console.log('All Game Data: ', allGameData);
         
         //for (let i = 0; i < allGameData.length; i++)
         //{
@@ -25,32 +30,23 @@ function start()
             .then(result => result.json())
             .then((monetizationData) => {
 
-                console.log();
-                if (monetizationData.Paid >= 1) changeCheckmarkColor(checkmarkCollection[0], "#ff9900");
-                if (monetizationData.PaidDLC >= 1) changeCheckmarkColor(checkmarkCollection[1], "#ff9900");
-                if (monetizationData.Microtransactions >= 1) changeCheckmarkColor(checkmarkCollection[2], "#ff9900");
-                if (monetizationData.Advertisements >= 1) changeCheckmarkColor(checkmarkCollection[3], "#ff9900");
-                if (monetizationData.SeasonPass >= 1) changeCheckmarkColor(checkmarkCollection[4], "#ff9900");
-                if (monetizationData.PremiumBattlePass >= 1) changeCheckmarkColor(checkmarkCollection[5], "#ff9900");
-                if (monetizationData.PremiumCurrency >= 1) changeCheckmarkColor(checkmarkCollection[6], "#ff9900");
-                if (monetizationData.PremiumLootBoxes >= 1) changeCheckmarkColor(checkmarkCollection[7], "#ff9900");
-                if (monetizationData.PremiumCosmetics >= 1) changeCheckmarkColor(checkmarkCollection[8], "#ff9900");
-                if (monetizationData.DeluxeEdition >= 1) changeCheckmarkColor(checkmarkCollection[9], "#ff9900");
-                if (monetizationData.PreorderBonus >= 1) changeCheckmarkColor(checkmarkCollection[10], "#ff9900");
-                if (monetizationData.Subscription >= 1) changeCheckmarkColor(checkmarkCollection[11], "#ff9900");
-                if (monetizationData.PayToWin >= 1) changeCheckmarkColor(checkmarkCollection[12], "#ff9900");
-                if (monetizationData.FOMOMechanics >= 1) changeCheckmarkColor(checkmarkCollection[13], "#ff9900");
-                if (monetizationData.NFTs >= 1) changeCheckmarkColor(checkmarkCollection[14], "#ff9900");
-                if (monetizationData.PlayerMarket >= 1) changeCheckmarkColor(checkmarkCollection[15], "#ff9900");
+                if (monetizationData.Paid >= 1) activateCheckmark(checkmarkCollection[0]);
+                if (monetizationData.PaidDLC >= 1) activateCheckmark(checkmarkCollection[1]);
+                if (monetizationData.Microtransactions >= 1) activateCheckmark(checkmarkCollection[2]);
+                if (monetizationData.Advertisements >= 1) activateCheckmark(checkmarkCollection[3]);
+                if (monetizationData.SeasonPass >= 1) activateCheckmark(checkmarkCollection[4]);
+                if (monetizationData.PremiumBattlePass >= 1) activateCheckmark(checkmarkCollection[5]);
+                if (monetizationData.PremiumCurrency >= 1) activateCheckmark(checkmarkCollection[6]);
+                if (monetizationData.PremiumLootBoxes >= 1) activateCheckmark(checkmarkCollection[7]);
+                if (monetizationData.PremiumCosmetics >= 1) activateCheckmark(checkmarkCollection[8]);
+                if (monetizationData.DeluxeEdition >= 1) activateCheckmark(checkmarkCollection[9]);
+                if (monetizationData.PreorderBonus >= 1) activateCheckmark(checkmarkCollection[10]);
+                if (monetizationData.Subscription >= 1) activateCheckmark(checkmarkCollection[11]);
+                if (monetizationData.PayToWin >= 1) activateCheckmark(checkmarkCollection[12]);
+                if (monetizationData.FOMOMechanics >= 1) activateCheckmark(checkmarkCollection[13]);
+                if (monetizationData.NFTs >= 1) activateCheckmark(checkmarkCollection[14]);
+                if (monetizationData.PlayerMarket >= 1) activateCheckmark(checkmarkCollection[15]);
 
-                // console.log('Monetization Data: ', monetizationData);
-
-                // console.log(checkmarkCollection);
-                // for (let i = 0; i < checkmarkCollection.length; i++)
-                // {
-                //     console.log(checkmarkCollection[i]);
-                //     changeCheckmarkColor(checkmarkCollection[i], "#ff9900");
-                // }
             }).catch(err => console.error(err));
             
             //element.innerHTML += '<div class="reviewCard"  onclick="goto(\'game.html\', \''+ output[i].gameID + '\')"><img class="reviewThumbnail" alt="Game Boxart" src="' + boxart + '"><p class="reviewTitle">' + title + '</p><p class="reviewRating">' + rating + '</p>	</div>';
@@ -59,6 +55,12 @@ function start()
 }
 
 function changeCheckmarkColor(svgElement, color) {
-    var svgItem = svgElement.contentDocument.getElementById("svgMain");
+    var svgItem = svgElement.getElementById("svgCheckmark");
 	svgItem.setAttribute("fill", color);
+}
+
+function activateCheckmark(element) {
+    element.classList.add("checkmarkActive");
+    element.children[0].style.display = "none";
+    element.children[1].style.display = "block";
 }
