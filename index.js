4b88c6b683444299249ae0d7ad85db3f0b6a77ff
keanine <keanen195@gmail.com>
2022-08-18 17:44:18 +0100

    PAGINATION BAYBEEEEEEEE

--- index.js
+++ index.js
@@ -1,27 +1,65 @@
 
+let reviewListElement;
+let gameDataIndex = 0;
+const gamesPerPage = 6;
+
+let database;
+
 function start()
 {
-    const element = document.getElementById("reviewList");
+    reviewListElement = document.getElementById("reviewList");
 
     fetch('./data/game_database.json')
     .then(result => result.json())
     .then((output) => {
-        console.log('Output: ', output);
-        
-        for (let i = 0; i < output.length; i++)
-        {
-            let title = output[i].title;
-            let rating = output[i].rating;
-            let boxart = output[i].boxart;
-            
-            element.innerHTML += '<div class="reviewCard"  onclick="goto(\'game\', \''+ output[i].gameID + '\')"><img class="reviewThumbnail" alt="Game Boxart" src="' + boxart + '"><p class="reviewTitle">' + title + '</p><p class="reviewRating">' + rating + '</p>	</div>';
-        }
+        database = output;
+        loadPage();
     }).catch(err => console.error(err));
 }
 
 function goto(page, arg)
 {
     window.location.href = page + '?id=' + arg;
+}
+
+function nextPage()
+{
+    if (gameDataIndex + gamesPerPage < database.length)
+    {
+        reviewListElement.innerHTML = "";
+        gameDataIndex += gamesPerPage;
+        loadPage();
+    }
+}
+
+function prevPage()
+{
+    if (gameDataIndex - gamesPerPage >= 0)
+    {
+        reviewListElement.innerHTML = "";
+        gameDataIndex -= gamesPerPage;
+        loadPage();
+    }
+}
+
+function loadPage()
+{
+    for (let i = gameDataIndex; i < gameDataIndex + gamesPerPage; i++)
+    {
+        let gameID = database[i].gameID;
+        let title = database[i].title;
+        let rating = database[i].rating;
+        let boxart = database[i].boxart;
+
+        if (i < database.length)
+        {
+            reviewListElement.innerHTML += '<div class="reviewCard"  onclick="goto(\'game\', \''+ gameID + '\')"><img class="reviewThumbnail" alt="Game Boxart" src="' + boxart + '"><p class="reviewTitle">' + title + '</p><p class="reviewRating">' + rating + '</p>	</div>';
+        }
+        else
+        {
+            return;
+        }
+    }
 }
 
     // Game Page
